c6d7dc26b8419c57ce0b3c6f827b087a0710d48b
bbzip <p.p.yoyo555@gmail.com>
2025-05-11 00:59:38 +0800

    Fix i18n localePath for Vercel compatibility

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
     localeDetection: false,
+    localePath: path.resolve('./public/locales'),
   },
-  localePath: './public/locales',
 }; 
